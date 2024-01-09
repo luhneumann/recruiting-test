49b46ed8b044c7ad198c12c250ad9fb8616766f7
luhneumann <luanafneumann@hotmail.com>
2024-01-09 14:07:46 -0300

    add data folder

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+$(document).ready(function(){
+  $('.slide-container').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll:4,
+    
+  });
+});
+
+fetch(
+  "/data/products.json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      console.log("===========")
+      console.table(element)
+      console.log("===========")
+
+      $('.slider-container').slick("slickAdd",
+      <div class="card">
+        <div class="like"></div>
+      </div>)
+      
+    });
+  })
 
 
 
